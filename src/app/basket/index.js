4487d2115f46a1f04129de69d51745a98f9e433e
Resmart <128597148+DunaevVladimir@users.noreply.github.com>
2023-05-26 16:50:51 +0300

    fix basket-item
    Вынес добавление ссылки у basket тоже

--- src/app/basket/index.js
+++ src/app/basket/index.js
@@ -28,7 +28,7 @@ function Basket() {
 
 	const renders = {
 		itemBasket: useCallback((item) => {
-			return <ItemBasket item={item} onRemove={callbacks.removeFromBasket} />
+			return <ItemBasket item={item} onRemove={callbacks.removeFromBasket} link={`/product/${item._id}`} />
 		}, [callbacks.removeFromBasket]),
 	};
 
